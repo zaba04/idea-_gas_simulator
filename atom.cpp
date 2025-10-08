fb76b5bde7f80891099f393b5ffa80eae2c8953d
zaba04 <kodymanna770@gmail.com>
2025-10-08 09:32:52 +0200

    first commit

---
+++ atom.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ann Kod on 08/10/2025.
+//
+
+#include "atom.h"
---
+++ atom.h
@@ -0,0 +1,16 @@
+//
+// Created by Ann Kod on 08/10/2025.
+//
+
+#ifndef ATOM_H
+#define ATOM_H
+
+
+
+class atom {
+
+};
+
+
+
+#endif //ATOM_H
