ea8b6640e720bb4c5aec882b93e003a9ad723f3e
zaba04 <kodymanna770@gmail.com>
2025-10-15 09:33:38 +0200

    starting work on algoritm 2

---
+++ algorytm_2.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ann Kod on 15/10/2025.
+//
+
+#include "algorytm_2.h"
---
+++ algorytm_2.h
@@ -0,0 +1,16 @@
+//
+// Created by Ann Kod on 15/10/2025.
+//
+
+#ifndef ALGORYTM_2_H
+#define ALGORYTM_2_H
+
+
+
+class algorytm_2 {
+
+};
+
+
+
+#endif //ALGORYTM_2_H
