48a84123941af0cfbcd3f46980799ec8a2e7e5ec
zaba04 <kodymanna770@gmail.com>
2025-10-15 11:01:57 +0200

    some optymalization work

--- algorytm_2.cpp
+++ algorytm_2.cpp
@@ -10,33 +10,56 @@ void algorytm_2::algo_2(std::vector<atom>& atoms, parameters& params, system_par
     double tmpVP = 0.0;
     double tmpFs = 0.0;
 
-    for (size_t i = 0; i < atoms.size(); i++) {
+    const double f = params.get_f();
+    const double l = params.get_l();
+    const double ee = params.get_ee();
+    const double r = params.get_r();
+    const double r_inv = 1.0 / r;
+
+    const size_t n = atoms.size();
+
+    for (size_t i = 0; i < n; i++) {
         //for that calculates Vs ad Vp and Fsi Fpi
         double tmpFsi = 0.0;
         double tmpFpi = 0.0;
 
         double tmpR = atoms[i].get_R();
 
-        if (tmpR >= params.get_l()) {
-            tmpVS += 0.5 * params.get_f() * (tmpR - params.get_l()) * (tmpR - params.get_l());
+        if (tmpR >= l) {
+            const double delta = tmpR - l;
+
+            tmpVS += 0.5 * f * delta * delta;
             //force from walls ov vessel
-            tmpFsi = params.get_f() * (params.get_l() - tmpR);
+            tmpFsi = f * (l - tmpR);
             tmpFs += fabs(tmpFsi);
         }
 
+        const double xi = atoms[i].get_x();
+        const double yi = atoms[i].get_y();
+        const double zi = atoms[i].get_z();
+
         for (size_t j = 0; j < i; j++) {
             //Vp section
+            double dx = xi - atoms[j].get_x();
+            double dy = yi - atoms[j].get_y();
+            double dz = zi - atoms[j].get_z();
+            const double r_sq = dx*dx + dy*dy + dz*dz;
 
-            double dx = atoms[i].get_x() - atoms[j].get_x();
-            double dy = atoms[i].get_y() - atoms[j].get_y();
-            double dz = atoms[i].get_z() - atoms[j].get_z();
-            double tmpRjk = sqrt(dx*dx + dy*dy + dz*dz);
 
-            if (tmpRjk > 1e-10) {  // Unikaj dzielenia przez 0
-                tmpVP += params.get_ee() * (pow(params.get_r()/tmpRjk, 12) - 2.0 * pow(params.get_r()/tmpRjk, 6));
 
-                double force_magnitude = 12.0 * params.get_ee() / tmpRjk *
-                    (pow(params.get_r()/tmpRjk, 12) - pow(params.get_r()/tmpRjk, 6));
+            if (r_sq > 1e-20) {  // Unikaj dzielenia przez 0
+                double tmpRjk = sqrt(r_sq);
+                const double ratio = r / tmpRjk;
+
+                const double ratio2 = ratio * ratio;        // r²
+                const double ratio6 = ratio2 * ratio2 * ratio2;  // r⁶
+                const double ratio12 = ratio6 * ratio6; //r^12
+
+                // Potencjał Lennarda-Jonesa
+                tmpVP += ee * (ratio12 - 2.0 * ratio6);
+
+                double force_magnitude = 12.0 * ee / tmpRjk *
+                    (ratio12 - ratio6);
                 tmpFpi += force_magnitude;
             }
         }
@@ -44,5 +67,5 @@ void algorytm_2::algo_2(std::vector<atom>& atoms, parameters& params, system_par
         atoms[i].set_F(tmpFsi, tmpFpi);
     }
     system.setV(tmpVS+tmpVP);
-    system.setP(1./(4*M_PI*params.get_l()*params.get_l()) * tmpFs);
+    system.setP(1./(4*M_PI*l*l) * tmpFs);
 }
